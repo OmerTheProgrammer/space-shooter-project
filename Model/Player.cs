45e108302fa61b814df2dd6320ffdcb62a098c94
OmerTheProgrammer <omer17508@gmail.com>
2025-09-29 00:02:20 +0300

    adding player

---
+++ Model/Player.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class PLayer : User
+    {
+        private int maxLevel;
+        private int totalScore;
+        private bool isSoundOn;
+        private bool isMusicOn;
+
+        public int MaxLevel { get => maxLevel; set => maxLevel = value; }
+        public int TotalScore { get => totalScore; set => totalScore = value; }
+        public bool IsSoundOn { get => isSoundOn; set => isSoundOn = value; }
+        public bool IsMusicOn { get => isMusicOn; set => isMusicOn = value; }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}" +
+                $"Max level : {this.MaxLevel},Total Score:{this.TotalScore}, " +
+                $"Sound is {this.IsSoundOn}ly on, " +
+                $"Music is {this.IsMusicOn}ly on.\n";
+        }
+    }
+}
---
+++ Model/PlayerList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class PLayerList : List<PLayer>
+    {
+        public PLayerList() { }
+        public PLayerList(IEnumerable<PLayer> list) : base(list) { }
+        public PLayerList(IEnumerable<BaseEntity> list) : base(list.Cast<PLayer>().ToList()) { }
+    }
+}
