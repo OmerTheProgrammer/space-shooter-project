1427d40101bca39404d9c2c487790d2e86362112
OmerTheProgrammer <omer17508@gmail.com>
2025-09-28 23:56:20 +0300

    manager

---
+++ Model/Manager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Manager : User
+    {
+        private DateTime startDate;
+
+        public DateTime StartDate { get => startDate; set => startDate = value; }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()} Start Date: {this.StartDate}.\n";
+        }
+    }
+}
