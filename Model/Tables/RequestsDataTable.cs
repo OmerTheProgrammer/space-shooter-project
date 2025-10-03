6e4fa633c50c6d99c403d6c84dfc893525c52ea9
OmerTheProgrammer <omer17508@gmail.com>
2025-10-03 20:13:04 +0300

    added RequestsDataTable

---
+++ Model/Tables/RequestsDataTable.cs
@@ -0,0 +1,16 @@
+﻿using Model.Entitys;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Tables
+{
+    public class RequestsDataTable : List<RequestData>
+    {
+        public RequestsDataTable() { }
+        public RequestsDataTable(IEnumerable<RequestData> list) : base(list) { }
+        public RequestsDataTable(IEnumerable<BaseEntity> list) : base(list.Cast<RequestData>().ToList()) { }
+    }
+}
