834052119e5276990d546a81fcc8a62dcfb63fc3
OmerTheProgrammer <omer17508@gmail.com>
2025-10-03 19:45:21 +0300

    added EnemyInLastLevel

---
+++ Model/Entitys/EnemyInLastLevel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Entitys
+{
+    enum Enemy
+    {
+        space_ship,//the basic green
+        space_destroyer, // red - not implemnted yet
+        mini_boss, // not implemnted yet
+        boss, // not implemnted yet
+    }
+
+    public class EnemyInLastLevel : BaseEntity
+    {
+        private RunInfo runInfo;
+        private Enemy name;
+        private int Amount;
+
+        public override string ToString()
+        {
+            return $"{base.ToString()} In run: {this.runInfo}.\n" +
+                $"there are {this.Amount} Enemy {this.name} ";
+        }
+    }
+}
