87d2ce2309adf05135d1a26487aca78769430803
OmerTheProgrammer <omer17508@gmail.com>
2025-10-03 15:42:15 +0300

    added PlayerAndGroup

---
+++ Model/Entitys/PlayerAndGroup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Entitys
+{
+    public class PlayerAndGroup : BaseEntity
+    {
+        private PLayer player;
+        private Group group;
+
+        public override string ToString()
+        {
+            return $"{base.ToString()} Group: {this.group}.\n" +
+                $"Player: {this.player}";
+        }
+    }
+}
