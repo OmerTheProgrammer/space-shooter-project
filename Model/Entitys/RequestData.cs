73ec4761b8aa38a941e6cdc2baf8a64c74e75437
OmerTheProgrammer <omer17508@gmail.com>
2025-10-03 20:11:13 +0300

    added request data

---
+++ Model/Entitys/RequestData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Entitys
+{
+    public class RequestData :BaseEntity
+    {
+        private ProfileEditRequest request;
+        private string field;
+        private string oldValue;
+        private string newValue;
+
+        public ProfileEditRequest Request { get => request; set => request = value; }
+        public string Field { get => field; set => field = value; }
+        public string OldValue { get => oldValue; set => oldValue = value; }
+        public string NewValue { get => newValue; set => newValue = value; }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}" +
+                $"Connected Request : {this.Request}, \n" +
+                $"Field: {this.Field}, " +
+                $"Old Value: {this.OldValue}, " +
+                $"New Value: {this.NewValue},\n";
+        }
+    }
+}
