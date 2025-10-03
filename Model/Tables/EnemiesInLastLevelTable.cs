8ddf63640f30f9c0e85004c07bd7553c68ed0af5
OmerTheProgrammer <omer17508@gmail.com>
2025-10-03 20:17:39 +0300

    added EnemiesInLastLevelTable

---
+++ Model/Tables/EnemiesInLastLevelTable.cs
@@ -0,0 +1,16 @@
+﻿using Model.Entitys;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Tables
+{
+    public class EnemiesInLastLevelTableTable : List<EnemyInLastLevel>
+    {
+        public EnemiesInLastLevelTableTable() { }
+        public EnemiesInLastLevelTableTable(IEnumerable<EnemyInLastLevel> list) : base(list) { }
+        public EnemiesInLastLevelTableTable(IEnumerable<BaseEntity> list) : base(list.Cast<EnemyInLastLevel>().ToList()) { }
+    }
+}
