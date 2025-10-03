3e2ab532e5cb70cc50548137b9eae9146610cdfa
OmerTheProgrammer <omer17508@gmail.com>
2025-10-03 20:16:05 +0300

    added ProfileEditRequestsTable

---
+++ Model/Tables/ProfileEditRequestsTable.cs
@@ -0,0 +1,16 @@
+﻿using Model.Entitys;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Tables
+{
+    public class ProfileEditRequestsTable : List<ProfileEditRequest>
+    {
+        public ProfileEditRequestsTable() { }
+        public ProfileEditRequestsTable(IEnumerable<ProfileEditRequest> list) : base(list) { }
+        public ProfileEditRequestsTable(IEnumerable<BaseEntity> list) : base(list.Cast<ProfileEditRequest>().ToList()) { }
+    }
+}
