e7ee8eb75ec21ace79b19f4c95159bd387266179
OmerTheProgrammer <omer17508@gmail.com>
2025-10-04 11:19:56 +0300

    added protactions in run info

--- Model/Entitys/RunInfo.cs
+++ Model/Entitys/RunInfo.cs
@@ -16,10 +16,62 @@ namespace Model.Entitys
         public Player Player { get => player; set => player = value; }
         public int CurrentScore { get => currentScore; set => currentScore = value; }
         public int CurrentLevel { get => currentLevel; set => currentLevel = value; }
-        public int CurrentShieldLevel { get => currentShieldLevel; set => currentShieldLevel = value; }
-        public int CurrentBlasterCount { get => currentBlasterCount; set => currentBlasterCount = value; }
+        public int CurrentShieldLevel {
+            get
+            {
+                return currentShieldLevel;
+            }
+            set
+            {
+                if(value >= 0)
+                {
+                    currentShieldLevel = value;
+                }
+                else
+                {
+                    currentShieldLevel = 0;
+                }
+            }
+        }
+
+        public int CurrentBlasterCount {
+            get
+            {
+                return currentBlasterCount;
+            }
+            set
+            {
+                if (value >= 1 && value <= 9)
+                {
+                    currentBlasterCount = value;
+                }
+                else
+                {
+                    currentBlasterCount = 1;
+                }
+            }
+        }
+
         public DateTime RunStopDate { get => runStopDate; set => runStopDate = value; }
-        public int CurrentHp { get => currentHp; set => currentHp = value; }
+        public int CurrentHp
+        {
+            get
+            {
+                return currentHp;
+            }
+            set
+            {
+                if (value >= 0)
+                {
+                    currentHp = value;
+                }
+                else
+                {
+                    //if hp removing goes negative set to 0, but defult is 5
+                    currentHp = 0;
+                }
+            }
+        }
         public bool IsRunOver { get => isRunOver; set => isRunOver = value; }
 
 
