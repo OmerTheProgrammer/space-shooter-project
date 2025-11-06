31ad6118423f132ebbfce4f9ad132e94951c21f7
OmerTheProgrammer <omer17508@gmail.com>
2025-11-06 10:49:25 +0200

    tried to add selectbyid to apiservice

--- Client Manager - API/ApiService.cs
+++ Client Manager - API/ApiService.cs
@@ -1,4 +1,5 @@
-﻿using Model.Tables;
+﻿using Model.Entitys;
+using Model.Tables;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,7 +24,7 @@ namespace Client_Manager___API
         public ApiService() : this("http://localhost:7013") { }
 
         #region select all:
-        private async Task<T> GetTableAsync<T>(string endpoint)
+        private async Task<T> GetTable<T>(string endpoint)
             where T : new()
         {
             try
@@ -42,47 +43,102 @@ namespace Client_Manager___API
 
         public async Task<AdminsTable> GetAllAdmins()
         {
-            return await GetTableAsync<AdminsTable>("/api/SelectAll/AdminsSelector");
+            return await GetTable<AdminsTable>("/api/SelectAll/AdminsSelector");
         }
 
         public async Task<EnemiesInLastLevelTable> GetAllEnemiesInLastLevel()
         {
-            return await GetTableAsync<EnemiesInLastLevelTable>("/api/SelectAll/EnemiesInLastLevelSelector");
+            return await GetTable<EnemiesInLastLevelTable>("/api/SelectAll/EnemiesInLastLevelSelector");
         }
 
         public async Task<GroupsTable> GetAllGroups()
         {
-            return await GetTableAsync<GroupsTable>("/api/SelectAll/GroupsSelector");
+            return await GetTable<GroupsTable>("/api/SelectAll/GroupsSelector");
         }
 
         public async Task<PlayersTable> GetAllPlayers()
         {
-            return await GetTableAsync<PlayersTable>("/api/SelectAll/PlayersSelector");
+            return await GetTable<PlayersTable>("/api/SelectAll/PlayersSelector");
         }
 
         public async Task<ProfileEditRequestsTable> GetAllProfileEditRequests()
         {
-            return await GetTableAsync<ProfileEditRequestsTable>("/api/SelectAll/ProfileEditRequestsSelector");
+            return await GetTable<ProfileEditRequestsTable>("/api/SelectAll/ProfileEditRequestsSelector");
         }
 
-        public async Task<RequestsDataTable> GetAllRequestsDataDB()
+        public async Task<RequestsDataTable> GetAllRequestsData()
         {
-            return await GetTableAsync<RequestsDataTable>("/api/SelectAll/RequestsDataSelector");
+            return await GetTable<RequestsDataTable>("/api/SelectAll/RequestsDataSelector");
         }
 
-        public async Task<RunsInfoTable> GetAllRunsInfoDB()
+        public async Task<RunsInfoTable> GetAllRunsInfo()
         {
-            return await GetTableAsync<RunsInfoTable>("/api/SelectAll/RunsInfoSelector");
+            return await GetTable<RunsInfoTable>("/api/SelectAll/RunsInfoSelector");
         }
 
-        public async Task<UsersTable> GetAllUsersDB()
+        public async Task<UsersTable> GetAllUsers()
         {
-            return await GetTableAsync<UsersTable>("/api/SelectAll/UsersSelector");
+            return await GetTable<UsersTable>("/api/SelectAll/UsersSelector");
         }
         #endregion
 
         #region Select by Id
+        private async Task<T> GetById<T>(string endpoint)
+            where T : new()
+        {
+            try
+            {
+                // The method uses the relative path defined in the public methods
+                return await client.GetFromJsonAsync<T>(endpoint);
+            }
+            catch (Exception ex)
+            {
+                // Centralized error logging
+                Console.WriteLine($"Error fetching data from {endpoint}: {ex.Message}");
+                // Return an empty instance of the table type T
+                return new T();
+            }
+        }
 
+        public Task<Admin> GetAdminById(int idx)
+        {
+            return GetById<Admin>($"/api/SelectByIdController/AdminsSelectorById/{idx}");
+        }
+
+        public Task<EnemyInLastLevel> GetEnemiesInLastLevelById(int idx)
+        {
+            return GetById<EnemyInLastLevel>($"/api/SelectByIdController/EnemiesInLastLevelSelectorById/{idx}");
+        }
+
+        public Task<Group> GetGroupsById(int idx)
+        {
+            return GetById<Group>($"/api/SelectByIdController/GroupsSelectorById/{idx}");
+        }
+
+        public Task<Player> GetPlayersById(int idx)
+        {
+            return GetById<Player>($"/api/SelectByIdController/PlayersSelectorById/{idx}");
+        }
+
+        public Task<ProfileEditRequest> GetProfileEditRequestsById(int idx)
+        {
+            return GetById<ProfileEditRequest>($"/api/SelectByIdController/ProfileEditRequestsSelectorById/{idx}");
+        }
+
+        public Task<RequestData> GetRequestsDataById(int idx)
+        {
+            return GetById<RequestData>($"/api/SelectByIdController/RequestsDataSelectorById/{idx}");
+        }
+
+        public Task<RunInfo> GetRunsInfoById(int idx)
+        {
+            return GetById<RunInfo>($"/api/SelectByIdController/RunsInfoSelectorById/{idx}");
+        }
+
+        public Task<User> GetUsersById(int idx)
+        {
+            return GetById<User>($"/api/SelectByIdController/UsersSelectorById/{idx}");
+        }
         #endregion
     }
 }
