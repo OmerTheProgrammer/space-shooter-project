109daa70a6c660a6c5c74a13b94ca135cb69b9cb
OmerTheProgrammer <omer17508@gmail.com>
2025-11-06 10:57:44 +0200

    /api/SelectByIdController/AdminsSelectorById/{idx} GetAdminById and the rest dosen't name like this parmater hidden in body
    tried to fix, Async jason doesn't like int or has porblam sdending int or has problem sending it and retruning something else

--- Client Manager - API/ApiService.cs
+++ Client Manager - API/ApiService.cs
@@ -83,13 +83,13 @@ namespace Client_Manager___API
         #endregion
 
         #region Select by Id
-        private async Task<T> GetById<T>(string endpoint)
+        private async Task<T> GetById<T>(string endpoint, int idx)
             where T : new()
         {
             try
             {
                 // The method uses the relative path defined in the public methods
-                return await client.GetFromJsonAsync<T>(endpoint);
+                return await client.GetFromJsonAsync<int>(endpoint, idx);
             }
             catch (Exception ex)
             {
@@ -102,42 +102,42 @@ namespace Client_Manager___API
 
         public Task<Admin> GetAdminById(int idx)
         {
-            return GetById<Admin>($"/api/SelectByIdController/AdminsSelectorById/{idx}");
+            return GetById<Admin>($"/api/SelectByIdController/AdminsSelectorById",idx);
         }
 
         public Task<EnemyInLastLevel> GetEnemiesInLastLevelById(int idx)
         {
-            return GetById<EnemyInLastLevel>($"/api/SelectByIdController/EnemiesInLastLevelSelectorById/{idx}");
+            return GetById<EnemyInLastLevel>($"/api/SelectByIdController/AdminsSelectorById", idx);
         }
 
         public Task<Group> GetGroupsById(int idx)
         {
-            return GetById<Group>($"/api/SelectByIdController/GroupsSelectorById/{idx}");
+            return GetById<Group>($"/api/SelectByIdController/AdminsSelectorById", idx);
         }
 
         public Task<Player> GetPlayersById(int idx)
         {
-            return GetById<Player>($"/api/SelectByIdController/PlayersSelectorById/{idx}");
+            return GetById<Player>($"/api/SelectByIdController/AdminsSelectorById", idx);
         }
 
         public Task<ProfileEditRequest> GetProfileEditRequestsById(int idx)
         {
-            return GetById<ProfileEditRequest>($"/api/SelectByIdController/ProfileEditRequestsSelectorById/{idx}");
+            return GetById<ProfileEditRequest>($"/api/SelectByIdController/AdminsSelectorById", idx);
         }
 
         public Task<RequestData> GetRequestsDataById(int idx)
         {
-            return GetById<RequestData>($"/api/SelectByIdController/RequestsDataSelectorById/{idx}");
+            return GetById<RequestData>($"/api/SelectByIdController/AdminsSelectorById", idx);
         }
 
         public Task<RunInfo> GetRunsInfoById(int idx)
         {
-            return GetById<RunInfo>($"/api/SelectByIdController/RunsInfoSelectorById/{idx}");
+            return GetById<RunInfo>($"/api/SelectByIdController/AdminsSelectorById", idx);
         }
 
         public Task<User> GetUsersById(int idx)
         {
-            return GetById<User>($"/api/SelectByIdController/UsersSelectorById/{idx}");
+            return GetById<User>($"/api/SelectByIdController/AdminsSelectorById", idx);
         }
         #endregion
     }
